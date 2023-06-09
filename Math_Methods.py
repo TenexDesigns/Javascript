c8049a5b84fc9adda4eba37e6a88f6009e273a15
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-09 17:46:17 +0300

    Create Math_Methods.py

---
+++ Math_Methods.py
@@ -0,0 +1,61 @@
+JavaScript provides several built-in Math methods that allow you to perform common mathematical operations. Here are some of the commonly used Math methods:
+
+Math.abs(x): Returns the absolute value of a number x.
+
+
+console.log(Math.abs(-5)); // Output: 5
+Math.round(x): Rounds a number x to the nearest integer.
+
+console.log(Math.round(4.7)); // Output: 5
+console.log(Math.round(4.2)); // Output: 4
+Math.ceil(x): Returns the smallest integer greater than or equal to a number x.
+
+console.log(Math.ceil(4.2)); // Output: 5
+Math.floor(x): Returns the largest integer less than or equal to a number x.
+
+console.log(Math.floor(4.7)); // Output: 4
+Math.random(): Returns a random number between 0 (inclusive) and 1 (exclusive).
+
+console.log(Math.random()); // Output: Random number between 0 and 1
+Math.max(x1, x2, ..., xn): Returns the largest of the given numbers.
+
+console.log(Math.max(3, 7, 2, 9)); // Output: 9
+Math.min(x1, x2, ..., xn): Returns the smallest of the given numbers.
+
+console.log(Math.min(3, 7, 2, 9)); // Output: 2
+Math.pow(x, y): Returns the value of x raised to the power of y.
+
+console.log(Math.pow(2, 3)); // Output: 8
+Math.sqrt(x): Returns the square root of a number x.
+
+
+console.log(Math.sqrt(16)); // Output: 4
+These are just a few examples of the Math methods available in JavaScript. 
+You can refer to the JavaScript documentation for a comprehensive list of Math methods and their detailed usage.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
