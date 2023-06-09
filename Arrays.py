75a741e34be9297c58ff46a9e72831ee7bbfd91c
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-06-09 19:55:42 +0300

    Update Arrays.py

--- Arrays.py
+++ Arrays.py
@@ -51,6 +51,27 @@ concat(): Combines two or more arrays.
 slice(): Returns a shallow copy of a portion of an array.
 splice(): Changes the contents of an array by removing, replacing, or adding elements.
 forEach(): Executes a provided function once for each array element.
+  
+JavaScript Array Methods
+
+Array length
+Array toString()
+Array pop()
+Array push()
+Array shift()
+Array unshift()
+Array join()
+Array delete()
+Array concat()
+Array flat()
+Array splice()
+Array slice()
+The methods are listed in the order they appear in this tutorial page
+The sort() method sorts an array alphabetically:
+
+
+
+Sorting an Array
 Iterating Over Arrays:
 Arrays can be iterated using loops, such as for loop or forEach() method.
 
@@ -65,4 +86,117 @@ for (let i = 0; i < numbers.length; i++) {
 numbers.forEach((number) => {
   console.log(number);
 });
-JavaScript arrays are versatile and widely used in web development for storing, manipulating, and accessing collections of data. They offer various methods and operations that make it convenient to work with ordered sets of values.
+JavaScript arrays are versatile and widely used in web development for storing, manipulating, and accessing collections of data.
+They offer various methods and operations that make it convenient to work with ordered sets of values.
+
+
+
+
+
+
+
+
+
+
+
+
+
+ITERATING OVER ARRAYS
+**************************************************************************************************
+
+
+JavaScript provides several methods for iterating over arrays and sorting their elements. Here are some commonly used array iteration and sorting methods:
+
+forEach() method:
+The forEach() method executes a provided function once for each element in an array.
+
+javascript
+Copy code
+const numbers = [1, 2, 3, 4, 5];
+
+numbers.forEach((number) => {
+  console.log(number);
+});
+map() method:
+The map() method creates a new array with the results of calling a provided function on every element in the original array.
+
+javascript
+Copy code
+const numbers = [1, 2, 3, 4, 5];
+
+const doubledNumbers = numbers.map((number) => {
+  return number * 2;
+});
+
+console.log(doubledNumbers); // Output: [2, 4, 6, 8, 10]
+filter() method:
+The filter() method creates a new array with all elements that pass a certain condition specified by a provided function.
+
+javascript
+Copy code
+const numbers = [1, 2, 3, 4, 5];
+
+const evenNumbers = numbers.filter((number) => {
+  return number % 2 === 0;
+});
+
+console.log(evenNumbers); // Output: [2, 4]
+reduce() method:
+The reduce() method applies a function to an accumulator and each element in the array (from left to right) to reduce it to a single value.
+
+javascript
+Copy code
+const numbers = [1, 2, 3, 4, 5];
+
+const sum = numbers.reduce((accumulator, number) => {
+  return accumulator + number;
+}, 0);
+
+console.log(sum); // Output: 15
+Sorting an Array:
+JavaScript arrays have a built-in sort() method that sorts the elements of an array in place and returns the sorted array.
+
+javascript
+Copy code
+const fruits = ['banana', 'apple', 'orange', 'grape'];
+
+fruits.sort();
+
+console.log(fruits); // Output: ['apple', 'banana', 'grape', 'orange']
+You can also use a compare function as an argument to the sort() method to define custom sorting behavior.
+
+javascript
+Copy code
+const numbers = [10, 2, 5, 8, 1];
+
+numbers.sort((a, b) => {
+  return a - b;
+});
+
+console.log(numbers); // Output: [1, 2, 5, 8, 10]
+These are just a few examples of array iteration and sorting methods available in JavaScript. Each method serves a specific purpose and can be used based on the requirements of your application.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
