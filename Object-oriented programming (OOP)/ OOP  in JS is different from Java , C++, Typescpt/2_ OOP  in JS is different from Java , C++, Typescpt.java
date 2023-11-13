5134e17e7a13a3cd0be0fc6610e5f60cf341b7ec
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-11-13 08:43:52 +0300

    Create 2_ OOP  in JS is different from Java , C++, Typescpt.java

---
+++ Object-oriented programming (OOP)/ OOP  in JS is different from Java , C++, Typescpt/2_ OOP  in JS is different from Java , C++, Typescpt.java
@@ -0,0 +1,28 @@
+When I mentioned that "In JavaScript, OOP is a bit different compared to languages like Java or C++, but it's a powerful paradigm nonetheless," I meant that JavaScript implements object-oriented programming (OOP) using a different paradigm compared to classical object-oriented languages like Java or C++. The differences lie in the way inheritance and object creation are handled.
+
+Here are some key points of distinction:
+
+1. **Prototypal Inheritance:**
+   - JavaScript uses prototypal inheritance, where objects can inherit properties and methods directly from other objects. This is different from classical languages like Java, which use class-based inheritance.
+
+2. **Objects and Functions:**
+   - In JavaScript, everything is an object, and functions are also objects. This is a departure from languages like Java, where functions (methods) are typically defined within classes.
+
+3. **Dynamic Typing:**
+   - JavaScript is dynamically typed, meaning you don't have to declare the type of a variable explicitly. This is unlike statically-typed languages like Java and C++.
+
+4. **First-Class Functions:**
+   - JavaScript treats functions as first-class citizens, allowing you to pass functions as arguments, return them from other functions, and assign them to variables. This flexibility is a distinctive feature of JavaScript.
+
+5. **Introduction of Classes:**
+   - ECMAScript 6 introduced the `class` syntax to make the syntax more familiar to developers coming from class-based languages. However, under the hood, JavaScript still uses prototypal inheritance.
+
+### TypeScript and OOP:
+
+TypeScript, being a superset of JavaScript, builds upon JavaScript's OOP features and introduces static typing, interfaces, and other features more commonly associated with classical OOP languages. By using TypeScript, developers can bring a more traditional class-based OOP experience to their JavaScript projects.
+
+So, its not a matter of JavaScript's OOP being "less than" other languages; it's just different. JavaScript's flexibility and prototypal nature can be powerful, but they may require a shift in mindset if you're more accustomed to classical OOP languages. TypeScript provides an option for developers who prefer a more classical OOP style while still enjoying the benefits of JavaScript's dynamic nature.
+
+
+
+  ..
